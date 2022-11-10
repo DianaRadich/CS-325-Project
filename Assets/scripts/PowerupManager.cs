c599bf46da1dc22e01535e6741a0d8a2cfdf1941
JordanRadich <jordanradich@gmail.com>
2022-11-10 17:13:33 -0500

    Switch statement for powerups

--- Assets/scripts/PowerupManager.cs
+++ Assets/scripts/PowerupManager.cs
@@ -24,12 +24,34 @@ public class PowerupManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if (Input.GetButtonDown("Fire3"))
+		{
+		    switch (powerup)
+		    {
+			    case Powerups.None:
+				    break;
+			    case Powerups.Dash:
+
+				    break;
+			    case Powerups.Jump:
+
+				    break;
+			    case Powerups.Shield:
+
+				    break;
+			    case Powerups.Recall:
+
+				    break;
+			    default:
+				    break;
+		    }
+		}
     }
 
     public void GetPowerUp(Powerups power){
         movmentScript.PickupBall();
-        if(power != Powerups.None){
+
+		if(power != Powerups.None){
             powerup = power;
         }
         
