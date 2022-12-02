34266a488413753f902587690de682349696762c
JordanRadich <jordanradich@gmail.com>
2022-12-01 20:27:53 -0500

    Recall powerup fixed

--- Assets/scripts/PowerupManager.cs
+++ Assets/scripts/PowerupManager.cs
@@ -89,7 +89,6 @@ public class PowerupManager : MonoBehaviour
                     GameObject ball = GameObject.FindGameObjectsWithTag("Ball")[0]; //there should only be one
                     GetPowerUp(ball.GetComponent<ProjectileBehaviour>().powerup);
                     Destroy(ball);
-                    powerup = Powerups.None;
                     animator.SetBool("isRed", false);
                     //does not set powerup because then recall wouldnt give palyer ball powerup
                     break;
